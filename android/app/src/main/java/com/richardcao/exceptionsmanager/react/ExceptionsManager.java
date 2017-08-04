42cf839df81cf1b69caafe0419202263f795e8ec
RichardCao <403164405@qq.com>
2017-08-04 15:48:38 +0800

    Adaptive react-native 0.47

--- android/app/src/main/java/com/richardcao/exceptionsmanager/react/ExceptionsManager.java
+++ android/app/src/main/java/com/richardcao/exceptionsmanager/react/ExceptionsManager.java
@@ -25,11 +25,6 @@ public class ExceptionsManager implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
