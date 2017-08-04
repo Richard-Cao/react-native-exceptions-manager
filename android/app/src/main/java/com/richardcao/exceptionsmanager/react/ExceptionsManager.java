7e4cd92a16b77251b3e8c5aac33f3ff74ff0eeb8
RichardCao <403164405@qq.com>
2017-08-04 16:27:57 +0800

    add annotation for createJSModules func

--- android/app/src/main/java/com/richardcao/exceptionsmanager/react/ExceptionsManager.java
+++ android/app/src/main/java/com/richardcao/exceptionsmanager/react/ExceptionsManager.java
@@ -24,6 +24,11 @@ public class ExceptionsManager implements ReactPackage {
         });
     }
 
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
